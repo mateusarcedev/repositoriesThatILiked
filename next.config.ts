d3021f78c628325fc5d83dddd3c65916b5889454
Mateus Arce <arcemateuss@gmail.com>
2024-11-15 01:45:37 -0400

    init

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: 'export',
   compress: false,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   experimental: {
     appDir: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
